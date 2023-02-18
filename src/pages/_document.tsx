ec66b6282b4cce4fb7546866097fbaa84e5150ca
Bayathy <misosoup0321@icloud.com>
2023-02-19 07:20:19 +0900

    chore: rename pages dir files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+// eslint-disable-next-line import/no-default-export
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
