be6979190e4e0760a868284908465ed27adbf535
iku.takagishi <i.takagishi@gmail.com>
2023-02-27 17:27:31 +0900

    little fix

--- src/samples/SampleEdgeAiRenponse.tsx
+++ src/samples/SampleEdgeAiRenponse.tsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 
 import { getEdgeAiStream } from '@/utils/getEdgeAiStream'
 
-export const SampleEdgeAiRenponse = async () => {
+export const SampleEdgeAiRenponse = () => {
   const [loading, setLoading] = useState<boolean>(false)
   const [output, setOutput] = useState<string>('')
   const prompt = '例）東京都のおすすめ観光地は？'
   return (
     <div>
-      <p>{output}</p>
       <button
         onClick={e => {
           e.preventDefault()
@@ -17,6 +16,7 @@ export const SampleEdgeAiRenponse = async () => {
       >
         submit
       </button>
+      <p>{output}</p>
     </div>
   )
 }
