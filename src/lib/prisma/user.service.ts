2fc3b1a52f4cc721d60b9476dd71fe4248664e78
Bayathy <misosoup0321@icloud.com>
2023-04-06 05:34:33 +0900

    feat: update prisma utils

--- src/lib/prisma/user.service.ts
+++ src/lib/prisma/user.service.ts
@@ -6,7 +6,12 @@ import { prisma } from '@/lib/prisma/prisma'
 export async function createUser(req: NextApiRequest, res: NextApiResponse) {
   const data: Prisma.UserCreateInput = req.body
   try {
-    const newUser = await prisma.user.create({ data })
+    const newUser = await prisma.user.create({
+      data,
+      include: {
+        history: true,
+      },
+    })
     return res.status(200).json({ data: newUser, success: true })
   } catch (error) {
     console.error('Request error', error)
@@ -17,7 +22,12 @@ export async function createUser(req: NextApiRequest, res: NextApiResponse) {
 export async function getUser(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
   try {
-    const user = await prisma.user.findUnique({ where: { id: Number(id) } })
+    const user = await prisma.user.findUnique({
+      where: { id: Number(id) },
+      include: {
+        history: true,
+      },
+    })
     return res.status(200).json({ data: user, success: true })
   } catch (error) {
     console.error('Request error', error)
@@ -26,10 +36,16 @@ export async function getUser(req: NextApiRequest, res: NextApiResponse) {
 }
 
 export async function updateUser(req: NextApiRequest, res: NextApiResponse) {
-  const data: Prisma.UserUpdateInput = req.body
+  const data = req.body
   const { id } = req.query
   try {
-    const newUser = await prisma.user.update({ where: { id: Number(id) }, data })
+    const newUser = await prisma.user.update({
+      where: { id: Number(id) },
+      data,
+      include: {
+        history: true,
+      },
+    })
     return res.status(200).json({ data: newUser, success: true })
   } catch (error) {
     console.error('Request error', error)
@@ -37,12 +53,12 @@ export async function updateUser(req: NextApiRequest, res: NextApiResponse) {
   }
 }
 
-export async function deleteUser(req: NextApiRequest, res: NextApiResponse){
+export async function deleteUser(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
   try {
-    const user = await prisma.user.delete({where:{id:Number(id)}})
-    return res.status(200).json({data: user, success: true})
-  }catch (error){
+    const user = await prisma.user.delete({ where: { id: Number(id) } })
+    return res.status(200).json({ data: user, success: true })
+  } catch (error) {
     console.error('Request error', error)
     res.status(500).json({ error: 'Error deleting data', success: false })
   }
